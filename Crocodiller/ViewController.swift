c29725027fcac881f50020d4ad0e8e6c9d971aa4
Яан Прокофьев <Dassam@Dassam.local>
2018-03-30 00:11:26 +0300

    Initial commit

---
+++ Crocodiller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Crocodiller
+//
+//  Created by Яан Прокофьев on 28.03.2018.
+//  Copyright © 2018 YandI. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    //var mainImage: UIImageView
+    //var newGameButton: UIButton
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
